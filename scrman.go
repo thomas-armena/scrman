1791d82053b64830d649ee0996a972dcd1fc8329
Thomas Armena <thomasarmena@gmail.com>
2021-06-28 15:32:49 -0400

    Create template for projects

--- scrman.go
+++ scrman.go
@@ -3,11 +3,22 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/gobuffalo/packr"
 )
 
 func main() {
-	print("Hello World\n")
 	err := initDirectories()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = initProject("helloworld")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = initProject("helloworld2")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,19 +39,67 @@ func initDirectories() error {
 
 	os.Chdir(homeDir)
 
-	err = os.MkdirAll(".scrman", 0755)
+	err = os.MkdirAll(".scrman", 0777)
 	if err != nil {
 		return err
 	}
 
 	os.Chdir(".scrman")
 
-	err = os.MkdirAll("bin", 0755)
+	err = os.MkdirAll("bin", 0777)
 	if err != nil {
 		return err
 	}
 
-	err = os.MkdirAll("scripts", 0755)
+	err = os.MkdirAll("scripts", 0777)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initProject(projectName string) error {
+
+	// TODO: validate project name
+
+	currDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(currDir)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	projectDir := homeDir + "/.scrman/scripts/" + projectName
+
+	err = os.MkdirAll(projectDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	os.Chdir(projectDir)
+
+	box := packr.NewBox("./templates")
+	index, err := box.Find("index.sh")
+	if err != nil {
+		return err
+	}
+
+	config, err := box.Find("config.json")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("index.sh", index, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("config.json", config, 0777)
 	if err != nil {
 		return err
 	}
